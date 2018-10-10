e7353d54f8a616cd1dc76d24034a1b135f863155
Matheus Castello <matheus@castello.eng.br>
2018-10-09 23:02:33 -0300

    script: fix color scapes
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- checkDepends.sh
+++ checkDepends.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-RED='\x1b[42;37m'
+GREEN='\x1b[42;37m'
+RED='\033[0;31m'
 NC='\033[0m' 
 
-echo -e "${RED}INSTALL BENCHMARK DEPENDENCIES${NC}"
+echo -e "${GREEN}INSTALL BENCHMARK DEPENDENCIES${NC}"
 echo "Author: Matheus Castello <matheus@mpro3.com.br>"
 echo "Version: 0.1"
 echo ""
 
 sudo apt-get install sysbench xvfb mesa-utils
 
-echo -e "${RED}MAKE AND INSTALL VIDEO CORE OPENGV EXAMPLES${NC}"
+echo -e "${GREEN}MAKE AND INSTALL VIDEO CORE OPENGV EXAMPLES${NC}"
 
 cd /opt/vc/src/hello_pi
 make
--- cpuinfo.sh
+++ cpuinfo.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-RED='\x1b[42;37m'
+GREEN='\x1b[42;37m'
+RED='\033[0;31m'
 NC='\033[0m' 
 
-echo -e "${RED}CPU INFO${NC}"
+echo -e "${GREEN}CPU INFO${NC}"
 echo "Author: Matheus Castello <matheus@mpro3.com.br>"
 echo "Version: 0.1"
 echo ""
 
-echo -e "${RED}CORE INFO${NC}"
+echo -e "${GREEN}CORE INFO${NC}"
 
 MODEL=`cat /proc/cpuinfo | grep "model name"`
 VENDOR=`cat /proc/cpuinfo | grep "Hardware"`
@@ -19,12 +20,12 @@ IFS=$':'; array2=($VENDOR); unset IFS;
 IFS=$'='; array3=($TEMPVC); unset IFS;
 
 echo "CORES:	${#array[@]}"
-echo "VENDOR: ${array2[1]}"
+echo "VENDOR:${array2[1]}"
 echo "TEMP:	$TEMP"
 echo "TEMPVC:	${array3[1]}"
 echo ""
 
-echo -e "${RED}RUN SYSBENCH${NC}"
+echo -e "${GREEN}RUN SYSBENCH${NC}"
 
 echo Running ...
 SYSBENCHRET=`sysbench --test=cpu --cpu-max-prime=2000 --num-threads=4 run`
@@ -32,11 +33,14 @@ echo Done
 
 TEMP=`cat /sys/class/thermal/thermal_zone0/temp`
 TEMPVC=`/opt/vc/bin/vcgencmd measure_temp`
+IFS=$'='; array3=($TEMPVC); unset IFS;
 
 echo ""
+echo -e "${RED}"
 echo "$SYSBENCHRET" | grep "total time:"
+echo -e "${NC}"
 echo ""
 echo "TEMP:	$TEMP"
-echo "TEMPVC:	$TEMPVC"
+echo "TEMPVC:	${array3[1]}"
 echo ""
 echo "END"
