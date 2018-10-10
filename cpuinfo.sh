02c9af7949c2b7a7edd725f0a8dfa4391c369ee4
Matheus Castello <matheus@castello.eng.br>
2018-10-09 21:54:23 -0300

    script: check temperature
    Add info from video temperature
    
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- cpuinfo.sh
+++ cpuinfo.sh
@@ -13,6 +13,7 @@ echo -e "${RED}CORE INFO${NC}"
 MODEL=`cat /proc/cpuinfo | grep "model name"`
 VENDOR=`cat /proc/cpuinfo | grep "hardware"`
 TEMP=`cat /sys/class/thermal/thermal_zone0/temp`
+TEMPVC=`/opt/vc/bin/vcgencmd measure_temp`
 IFS=$'\n'; array=($MODEL); unset IFS;
 
 echo "CORES:	${#array[@]}"
@@ -23,8 +24,12 @@ echo ""
 echo -e "${RED}RUN SYSBENCH${NC}"
 
 echo Running ...
-SYSBENCHRET=`sysbench --test=cpu --cpu-max-prime=20000 --num-threads=4 run`
+SYSBENCHRET=`sysbench --test=cpu --cpu-max-prime=2000 --num-threads=4 run`
 echo Done
+
+TEMP=`cat /sys/class/thermal/thermal_zone0/temp`
+TEMPVC=`/opt/vc/bin/vcgencmd measure_temp`
+
 echo ""
 echo "$SYSBENCHRET" | grep "total time"
 echo ""
