a94bbfff50bb086224df1fa21f4353f47ef77aee
Matheus Castello <matheus@castello.eng.br>
2018-10-30 12:11:54 -0300

    Dependencies: video core builds
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- checkDepends.sh
+++ checkDepends.sh
@@ -9,7 +9,27 @@ echo "Author: Matheus Castello <matheus@mpro3.com.br>"
 echo "Version: 0.1"
 echo ""
 
-sudo apt-get install sysbench xvfb mesa-utils
+sudo apt-get install sysbench xvfb mesa-utils git g++ build-essential pkg-config -y
+sudo apt-get install libx11-dev libgl1-mesa-dev -y
+sudo apt-get install libjpeg-dev libpng-dev -y
+
+echo -e "${GREEN}MAKE AND INSTALL VIDEO CORE OPENGV EXAMPLES${NC}"
+
+cd ~
+git clone https://github.com/glmark2/glmark2.git
+cd glmark2/
+
+echo "config ..."
+./waf configure --with-flavors=x11-gl
+
+echo "building ..."
+./waf build -j 4
+
+echo "installing ..."
+sudo ./waf install
+sudo strip -s /usr/local/bin/glmark2
+
+echo ""
 
 echo -e "${GREEN}MAKE AND INSTALL VIDEO CORE OPENGV EXAMPLES${NC}"
 
